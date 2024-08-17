2d2f1fd7a9f908848df47f7d64d2d94bbb164885
admin <admin@192.168.0.5>
2024-08-17 18:03:56 +0530

    basic java codes

---
+++ JavaTraining/src/javapackage/Average.java
@@ -0,0 +1,16 @@
+package javapackage;
+
+public class Average {
+
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = 6;
+		int c = 7;
+		int d = 8;
+		int e = 9;
+		int Avg = (a+b+c+d+e)/5;
+		System.out.println("Average of 5 numbers are "+Avg);
+	}
+
+}
---
+++ JavaTraining/src/javapackage/Sub.java
@@ -0,0 +1,13 @@
+package javapackage;
+
+public class Sub {
+
+	public static void main (String[] args) {
+
+		int a = 10;
+		int b = 20;
+		int Sub = a - b;
+		System.out.println("The difference of two numbers"+ Sub);
+	}
+
+}
